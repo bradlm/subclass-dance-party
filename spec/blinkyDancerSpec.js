1eca1cc0f2d5b59a2a9d28e0167f99c7aa545b26
bradlm <bradmelluish@gmail.com>
2016-08-01 17:17:01 -0700

    Implement Dancer and Blinky Dancer
    Refactor Dancer and BlinkyDancer to pseudoclassical, complete methods, fix tests.

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -5,7 +5,7 @@ describe('blinkyDancer', function() {
 
   beforeEach(function() {
     clock = sinon.useFakeTimers();
-    blinkyDancer = makeBlinkyDancer(10, 20, timeBetweenSteps);
+    blinkyDancer = new BlinkyDancer(10, 20, timeBetweenSteps);
   });
 
   it('should have a jQuery $node object', function() {
@@ -22,11 +22,11 @@ describe('blinkyDancer', function() {
     it('should call step at least once per second', function() {
       sinon.spy(blinkyDancer, 'step');
       expect(blinkyDancer.step.callCount).to.be.equal(0);
-    blinkyDancer = makeBlinkyDancer(10, 20, timeBetweenSteps);
+ /*     blinkyDancer = new BlinkyDancer(10, 20, timeBetweenSteps);
+      sinon.spy(blinkyDancer, 'step');*/ // can also use the commented out code, but it's redundant.
       clock.tick(timeBetweenSteps);
-
+      clock.tick(timeBetweenSteps);
       expect(blinkyDancer.step.callCount).to.be.equal(1);
-
       clock.tick(timeBetweenSteps);
       expect(blinkyDancer.step.callCount).to.be.equal(2);
     });
